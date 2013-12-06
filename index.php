0b7f129a04577c48f8f29cc83e1e3702d825dde6
unknown <C:\Documents and Settings\Admin\Application Data\The Bat!>
2013-12-06 10:58:32 +0200

    moved yii to root folder and changed bootstrap fie accordingly

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 // change the following paths if necessary
-//echo $_SERVER['PHP_SELF'];
-//$yii_root = dirname(__FILE__);//.'/../yii/framework/yii.php';
+$yii = dirname(__FILE__).'/../yii/framework/yii.php';
+/*
 $yii_path = str_replace('\\', '/', dirname(__FILE__));
 $yii_root = pathinfo($yii_path);
 $yii = $yii_root['dirname'].'/../public/yii/framework/yii.php';
+*/
 $config = dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
